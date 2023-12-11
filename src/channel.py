710a4229f045225f798af0fe3c9e591f6fc97794
Sergey_Shershikov <ssher250110@yandex.ru>
2023-12-11 21:00:55 +0300

    editing class Channel

--- src/channel.py
+++ src/channel.py
@@ -9,12 +9,15 @@ load_dotenv()
 
 class Channel:
     """Класс для ютуб-канала"""
+    # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
     api_key = os.getenv("YT_API_KEY")
+    # создать специальный объект для работы с API
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        # получение данные о канале по его id
         self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
 
     def printj(self, dict_to_print: dict) -> None:
